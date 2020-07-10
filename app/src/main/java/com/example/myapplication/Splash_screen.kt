e4e5090f352dda5bc5f49d47ffda3e36895ea2f8
igurmansingh <S.gurman28@23>
2020-07-09 20:00:46 -0400

    Version- Delta(1.0) Delta version of application is ready.

---
+++ app/src/main/java/com/example/myapplication/Splash_screen.kt
@@ -0,0 +1,34 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.Settings
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import okhttp3.internal.wait
+
+class Splash_screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        val i= Intent(this,MainActivity::class.java)
+        GlobalScope.launch {
+            withContext(Dispatchers.IO){
+                waits()
+            }
+            withContext(Dispatchers.Main){
+                startActivity(i)
+            }
+        }
+    }
+}
+fun waits(){
+    var a=System.currentTimeMillis()
+    while (a+2200>System.currentTimeMillis()){
+
+    }
+}
